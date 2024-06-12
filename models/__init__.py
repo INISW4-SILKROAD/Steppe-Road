5d2e7e94b490c047fbee51e198e6e8977b84123a
esillileu yun <esillileu@gmail.com>
2024-06-12 17:27:18 +0900

    종속성에 따른 파일 위치 변경

---
+++ models/__init__.py
@@ -0,0 +1,7 @@
+import os, sys
+
+dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if dir not in sys.path:
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from models.classifier import *
