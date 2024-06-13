5ab2570c580bbf9c0177dae99c39ff26d717a80c
esillileu yun <esillileu@gmail.com>
2024-06-13 13:11:17 +0900

    파일명 변경으로 인한 업데이트

--- models/constantinople.py
+++ models/constantinople.py
@@ -2,21 +2,43 @@ import torch.nn as nn
 
 # imagebind
 import encoder.custom_ibvis_encoder as cibv
-import encoder.custom_autoencoder as cae
+import models.encoder.simple_ae as cae
 
 class Constantinople(nn.Module):
-    def __init__(self, vision_embed_dim = 512, portion_dim = 12, output_dim = 4):
+    def __init__(self, latent_dim = 512, portion_dim = 12, touch_dim = 4):
         super(Constantinople, self).__init__()
-        self.image_encoder = cibv.CustomIbvisEncoder(out_embed_dim=vision_embed_dim)
-        self.portion_encoder = cae.CustomAutoEncoder().encoder
-        self.polling = nn.Bilinear(vision_embed_dim, vision_embed_dim, vision_embed_dim)
-        self.touch_decoder = cae.CustomAutoEncoder(4).decoder
+        self.image_encoder = cibv.CustomIbvisEncoder(out_embed_dim=latent_dim)
+        self.portion_encoder = cae.SimpleAE(
+            input_dim=portion_dim, 
+            latent_dim=latent_dim
+            ).encoder
+        
+        self.polling = nn.Bilinear(
+            in1_features=latent_dim, 
+            in2_features=latent_dim, 
+            out_features=latent_dim
+            )
+        
+        self.postprocess = nn.Sequential(
+            nn.Dropout(p=0.5),
+            nn.Linear(latent_dim, latent_dim), 
+            nn.BatchNorm1d(latent_dim),
+            nn.ReLU()  
+            )        
+
+        
+        self.touch_decoder = cae.SimpleAE(
+            input_dim=touch_dim,
+            latent_dim=latent_dim
+            ).decoder
         
         
     def forward(self, vision, portion):
         vision = self.image_encoder(vision)
         portion = self.portion_encoder(portion)
+        
         latent = self.polling(vision, portion)
+        latent = self.postprocess(latent)
+        
         result = self.touch_decoder(latent)
         return result
-    
