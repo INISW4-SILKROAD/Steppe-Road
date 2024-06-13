21a963cb2aa35a6ba4bc4fad1501ee0efae43e44
esillileu yun <esillileu@gmail.com>
2024-06-13 13:08:58 +0900

    함수명 변경으로 인한 업데이트

--- models/__init__.py
+++ models/__init__.py
@@ -4,5 +4,5 @@ dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 if dir not in sys.path:
     sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from models.classifier import *
-from models.encoder import *
+from models.classifier import custom_mobile_net, preprocessor
+from models.encoder import custom_ib_model, clip_vis_encoder, custom_ibvis_encoder, custom_multimodal_encoder, simple_ae, simple_vae
